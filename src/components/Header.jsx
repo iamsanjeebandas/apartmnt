294c7f5d9c305129c0dee185b856e8447d56f3f5
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-06-20 00:58:25 +0530

    created the frontend of the header.jsx file.

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-f4d35e">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-0d3b66">apartmnt</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-24"
+          />
+          <FaSearch className="text-slate-500 flex items-center" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-0d3b66 hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-0d3b66 hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+
+          <Link to="/signin">
+            <li className="hidden sm:inline text-0d3b66 hover:underline cursor-pointer">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "0d3b66": "#0d3b66",
+        "f4d35e": "#f4d35e",
+      },
+    },
   },
   plugins: [],
 };
