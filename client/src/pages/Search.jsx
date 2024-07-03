ec6af98acff625f0a5e56e1d62df03c459c0c2bd
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-07-04 00:35:11 +0530

    add onchange and onsubmit functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,8 +16,6 @@ export default function Search() {
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
 
-  console.log(listings);
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get("searchTerm");
